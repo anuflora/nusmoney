3d8f973f932fb256f9b89ffd86606271715a9dc8
anuflora <anuflora@users.noreply.github.com>
2021-03-17 03:02:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// (c) Anuflora Systems 
 const balance = document.getElementById('balance');
 const money_plus = document.getElementById('money-plus');
 const money_minus = document.getElementById('money-minus');
